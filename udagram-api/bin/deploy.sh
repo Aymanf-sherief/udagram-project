d1b7488708579fc8ba87ec96018c2ead0f858f98
Ayman Sherief <aymanf.sherief@gmail.com>
2022-03-24 15:33:45 +0200

    addressed review comments

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,8 @@
 cd www
-eb use prod
+eb use prod 
+
+echo "updating elastic beanstalk prod environment" 
+eb setenv JWT_SECRET=$JWT_SECRET AWS_BUCKET=$AWS_BUCKET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST  POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+
+echo "deploying to elastic beanstalk prod environment"
 eb deploy
